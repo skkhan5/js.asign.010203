d11874af74204debca1d96137b40a9de96819bb1
skkhan5 <yousafzai5588@gmail.com>
2024-12-18 22:27:26 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,62 @@
+alert("Error please enter a valid password");
+alert("Welcome to js land...  \n Happy coding");
+alert("Welcome to JS land...");
+alert("Happy coding! \n prevent this from creating additional diologs")
+alert("Hello... i can run JS through my web browser's console")
+
+// chapter 1 complete
+
+let myname = "Name : abdul samad khan";
+alert(myname)
+
+let myage = "my age : 25";
+alert(myage)
+
+let myprofession = "certified mobile application developer";
+alert(myprofession)
+
+let pizza = " pizza \n pizz \n piz \n pi \n p"
+alert(pizza)
+
+let myemail = "my email is " + "yousafzai531@gmail.com";
+alert(myemail)
+
+let book = "I m trying ro learn from book A smarter way to learn JavaScript";
+alert(book)
+
+// let display = "yeah i can write HTML content though javascript \n   and";
+// document.write(display);
+
+let samble = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+alert(samble);
+
+
+// chapter 2 complete
+
+let age = 25;
+alert(`I m ${age} years old`);
+
+
+
+for(let visitor = 0; visitor <5; visitor++){
+   alert(`I m visit my store website ${visitor} times`)
+
+}
+
+
+let birthyear = 1999;
+document.write(`yeah i can write HTML content though javascript <br /> my birthyaer ${birthyear} data type of my declear variable is number`)
+
+
+// let storedotcom = "storedotcom";
+// let tshart = "t-shart";
+// let qautiti = 0;
+// for( qautiti = 0; qautiti < 10; qautiti++ ){
+    
+//    console.log(`i m buying ${storedotcom} ${tshart} ${qautiti}`)
+
+// }
+// function count(){
+//     document.getElementById("click").innerHTML = `i m buying ${storedotcom} ${tshart} ${qautiti}`
+
+// }
